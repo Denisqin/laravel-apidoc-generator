b0d4e825f23927448d23aa8cff61e49250c034b6
denisqin <makevars@gmail.com>
2019-02-21 17:47:22 +0300

    Update info.blade.php

--- resources/views/partials/info.blade.php
+++ resources/views/partials/info.blade.php
@@ -1,6 +1,15 @@
 # Info
 
-Welcome to the generated API reference.
-@if($showPostmanCollectionButton)
-[Get Postman Collection]({{url($outputPath.'/collection.json')}})
-@endif
+https://habr.com/ru/post/351890/
+
+`A
+ id | value
+`
+`B
+ id | value
+` 
+`C
+ id | a_id  | b_id  | value
+`
+При составной сущности C = A +B, где составной ключ (a_id, b_id), URL будет следующий:
+api/a/{a_id}/b/{b_id} (POST,GET,PUT,DELETE)
